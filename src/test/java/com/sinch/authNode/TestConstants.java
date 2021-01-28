dd3aa1d11fc41af86b386a48f95d4234fad416f4
Aleksander Wojcik <aleksander.wojcik@sinch.com>
2021-01-28 11:36:34 +0100

    Code Collector tests added

--- src/test/java/com/sinch/authNode/TestConstants.java
+++ src/test/java/com/sinch/authNode/TestConstants.java
@@ -7,6 +7,7 @@ public class TestConstants {
     public static final String FAKE_APP_HASH = "appHash";
     public static final String FAKE_ID = "id";
     public static final String FAKE_NUM = "+123456789";
+    public static final String FAKE_CODE = "1234";
     public static final VerificationMethodType FAKE_METHOD = VerificationMethodType.SMS;
 
 }
